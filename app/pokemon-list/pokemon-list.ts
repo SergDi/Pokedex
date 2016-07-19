27edcf917498f3e44eecf6dce69c0e86a6cab3d0
sergkondrashov <sergkondrashov@gmail.com>
2016-07-20 01:12:25 +0300

    refactoring pokemonsList

--- app/pokemon-list/pokemon-list.ts
+++ app/pokemon-list/pokemon-list.ts
@@ -4,41 +4,40 @@ class PokemonListController {
 
     public pokemons: any[];
     public next: string;
-    public pokemon;   
+    public pokemon;
     public search;
 
     static $inject = ['$http'];
 
     constructor(public $http) {
-        this.init();     
+        this.pokemons = [];
+        this.init();
     }
 
     init() {
-        this.$http.get('http://pokeapi.co/api/v1/pokemon/?limit=12')
-            .then(response => {
-                this.next = response.data.meta.next;
-                this.pokemons = response.data.objects;
-            });
+        this.$http.get('http://pokeapi.co/api/v1/pokemon/?limit=12').then(this.load);
     }
 
     loadMore() {
-        this.$http.get(`http://pokeapi.co/${this.next}`)
-            .then(response => {
-                this.next = response.data.meta.next;
-                this.pokemons = this.pokemons.concat(response.data.objects);
-            });
+        this.$http.get(`http://pokeapi.co/${this.next}`).then(this.load);
     }
-    
-    setPokemon(pokemon){
-         this.pokemon = pokemon;
+
+    setPokemon(pokemon) {
+        this.pokemon = pokemon;
     }
-    
-    filterByType = (item) =>{
-           if(this.search)
-                return item.types.some( f => { return f.name.indexOf(this.search) != -1; });
-            else           
-               return true;
-       }
+
+    filterByType = (item) => {
+        if (this.search)
+            return item.types.some(f => { return f.name.indexOf(this.search) != -1; });
+        else
+            return true;
+    }
+
+    private load = (response) => {
+        this.next = response.data.meta.next;
+        this.pokemons = this.pokemons.concat(response.data.objects);
+    }
+
 }
 
 var PokemonListComponent = {
